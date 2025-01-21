add84241180383ee9551b7c2812b12ee7ed0cf91
820574911@qq.com <820574911@qq.com>
2025-01-21 17:55:49 +0800

    安卓端项目创建

---
+++ Axonix_App/app/src/main/java/com/Axonix/MyApplication.java
@@ -0,0 +1,21 @@
+package com.Axonix;
+
+import android.app.Application;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // ARouter 初始化
+        if (BuildConfig.DEBUG) {
+            ARouter.openLog();     // 打开日志
+            ARouter.openDebug();   // 调试模式
+        }
+
+        ARouter.init(this);
+
+    }
+}
